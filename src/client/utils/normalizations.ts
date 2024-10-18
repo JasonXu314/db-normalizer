1bfbd91c850e48c23fa4a61a2cd3b8706f41bcd5
Jason Xu <Xinwen31415@gmail.com>
2024-10-18 11:37:05 -0500

    finished 3nf normalization

--- src/client/utils/normalizations.ts
+++ src/client/utils/normalizations.ts
@@ -11,10 +11,12 @@ export function normalize(tables: ITable<any>[], fds: FD[], to: NF): ITable<any>
 	switch (to) {
 		case '1NF':
 			return normalize1NF(tables, fds);
+		case '3NF':
+			return normalize3NF(tables, fds);
 	}
 }
 
-export function normalize1NF(tables: NF0Table[], fds: FD[]): NF1Table[] {
+export function normalize1NF(tables: NF0Table[], _: FD[]): NF1Table[] {
 	const out = [];
 
 	tables.forEach((t) => {
@@ -23,9 +25,9 @@ export function normalize1NF(tables: NF0Table[], fds: FD[]): NF1Table[] {
 
 		table.names.forEach((colName) => {
 			if (Array.isArray(table.cols[colName][0]) && !table.pkey.includes(colName)) {
-				const keys = table.pkey.concat(colName);
+				const newCols = table.pkey.concat(colName);
 
-				const newTable = new Table(keys, Object.fromEntries(keys.map((k) => [k, []])), 0, -1, -1, table.pkey.slice());
+				const newTable = new Table(newCols, Object.fromEntries(newCols.map((k) => [k, []])), 0, -1, -1, table.pkey.slice());
 
 				for (let i = 0; i < table.length; i++) {
 					const tuple = table.get(i);
@@ -38,6 +40,44 @@ export function normalize1NF(tables: NF0Table[], fds: FD[]): NF1Table[] {
 
 				out.push(newTable);
 				table.removeCol(colName);
+			}
+		});
+	});
+
+	return out;
+}
+
+export function normalize3NF(tables: NF2Table[], fds: FD[]): NF3Table[] {
+	const out: NF3Table[] = [];
+
+	tables.forEach((t) => {
+		const table = t.clone();
+		out.push(table);
+
+		fds.forEach((fd) => {
+			if (
+				fd.determinant.some((col) => table.names.includes(col) && !table.pkey.includes(col)) &&
+				fd.dependent.some((col) => table.names.includes(col) && !table.pkey.includes(col))
+			) {
+				const toRemove = fd.dependent.filter((col) => table.names.includes(col) && !table.pkey.includes(col));
+				const newCols = fd.determinant.concat(toRemove);
+
+				const seen: string[] = [];
+				const newTable: NF3Table = new Table(newCols, {}, 0, -1, -1, fd.determinant.slice());
+
+				for (let i = 0; i < table.length; i++) {
+					const tuple = table.get(i);
+					const determinant = JSON.stringify(fd.determinant.map((col) => tuple[col]));
+
+					if (!seen.includes(determinant)) {
+						const newTuple = Object.fromEntries(newCols.map((key) => [key, tuple[key]]));
+
+						newTable.insert([newTuple]);
+						seen.push(determinant);
+					}
+				}
+
+				tables.push(newTable);
 			}
 		});
 	});
